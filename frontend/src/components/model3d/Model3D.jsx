87bf734ff0d8dd954b351a699027a8f10a9ed910
Van <salamanca.valentina@correounivalle.edu.co>
2023-05-23 18:00:18 -0500

    Update Model3D.jsx

--- frontend/src/components/model3d/Model3D.jsx
+++ frontend/src/components/model3d/Model3D.jsx
@@ -4,20 +4,39 @@ import Religion3D from './Religion3D';
 import { useRef } from 'react'
 
 const Model3D = () => {
-
     const directionalLightRef1 = useRef();
     const directionalLightRef2 = useRef();
 
-  return (
-    <>
-      <OrbitControls makeDefault minDistance={20} maxDistance={30} />
-      <directionalLight ref={directionalLightRef1} castShadow={true} position={[0, 20, 0]} intensity={0.3} />
-      <directionalLight ref={directionalLightRef2} castShadow={true} position={[0, -10, 0]} intensity={0.1} />
-      <ambientLight intensity={0.5} />
+    const handleMouseClick = () => {
+        const popupWindow = window.open('', '_blank', 'width=500,height=500');
+        if (popupWindow) {
+            popupWindow.document.write(`
+                <html>
+                <head>
+                    <title>Pop-up Window</title>
+                    <style>
+                        /* Estilos CSS para la ventana emergente */
+                    </style>
+                </head>
+                <body>
+                    <h1>Contenido de la ventana emergente</h1>
+                    <p>Este es el contenido de la ventana emergente.</p>
+                </body>
+                </html>
+            `);
+        }
+    };
 
-      <Religion3D scale="3.9" position={[0, 0, 0]} />
-    </>
-  );
+    return (
+        <>
+            <OrbitControls makeDefault minDistance={20} maxDistance={30} />
+            <directionalLight ref={directionalLightRef1} castShadow={true} position={[0, 20, 0]} intensity={0.3} />
+            <directionalLight ref={directionalLightRef2} castShadow={true} position={[0, -10, 0]} intensity={0.1} />
+            <ambientLight intensity={0.5} />
+
+            <Religion3D scale="3.9" position={[0, 0, 0]} onClick={handleMouseClick} />
+        </>
+    );
 };
 
 export default Model3D;
