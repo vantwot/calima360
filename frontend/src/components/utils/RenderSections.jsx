7038e0170acab4643450fc802a47dfde220aca5b
Cristian Duvan Machado Mosquera <62184928+CriistiianDM@users.noreply.github.com>
2023-07-08 02:46:45 -0500

    se hizo el porcentaje por leccion

--- frontend/src/components/utils/RenderSections.jsx
+++ frontend/src/components/utils/RenderSections.jsx
@@ -20,7 +20,11 @@ const RenderSections = ({
     const history =  useNavigate();
 
     //funcion para redireccionar
-    const handleCerrarSesion = ({target , pathBack , state , whoami}) => {
+    const handleCerrarSesion = ({target , pathBack , state , whoami , title}) => {
+
+        countClikLeccion({
+            item_click: title
+        })
 
         if(state) {
             state_use.set(!state_use.get)
@@ -31,9 +35,82 @@ const RenderSections = ({
         history(target, {  state: { pathBack } });
     }
 
+    const countClikLeccion = ({
+        item_click,
+        max_click
+    }) => {
+
+        try {
+
+            if ('countClick' in sessionStorage) {
+
+                const countClick = JSON.parse(sessionStorage.getItem('countClick'))
+
+                Object.entries(countClick).forEach( (element,index) => {
+
+                    Object.entries(element[1]).map( (element_) => {
+                        
+                        if (element_[0] === (item_click).toLowerCase()) {
+                            (countClick[element[0]])[element_[0]] = 1
+                        }
+
+                    })
+
+                   
+
+                });
+
+                sessionStorage.setItem('countClick' , JSON.stringify(countClick))
+
+            }
+            else {
+
+                sessionStorage.setItem('countClick' , JSON.parse({
+                    'orfebrería': {
+                        'arte': 0,
+                        'cultura': 0
+                    },
+                    'historia': {
+                        'vida': 0,
+                        'religión': 0
+                    },
+                    'mitología': {
+                        'mitología': 0
+                    },
+                }))
+
+            }
+
+        }
+        catch (error) {
+            console.log('error', error)
+        }
+
+    }
+
+
     //el todo poderoso useEffect
     React.useEffect(() => {
         document.getElementById('root').classList.add('remove_gap');
+
+        if (!('countClick' in sessionStorage)) {
+
+            sessionStorage.setItem('countClick' , JSON.stringify({
+                'orfebrería': {
+                    'arte': 0,
+                    'cultura': 0
+                },
+                'historia': {
+                    'vida': 0,
+                    'religión': 0
+                },
+                'mitología': {
+                    'mitología': 0
+                },
+            }))
+
+        }
+
     }, []);
 
     //render
@@ -51,7 +128,9 @@ const RenderSections = ({
                                             <p>{item.title}</p>
                                     </div>
                                     <ProgresQuestion name={item.title} 
-                                                     state_={item?.stateQuestion || false} />
+                                                     state_={item?.stateQuestion || false} 
+                                                     type={item?.type || 1}
+                                                     />
                                 </div>
                             )
                         })
