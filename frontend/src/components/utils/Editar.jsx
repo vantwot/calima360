a30b6ed927675bf601c501c58350f5d6791e4877
Cristian Duvan Machado Mosquera <62184928+CriistiianDM@users.noreply.github.com>
2023-07-08 01:04:48 -0500

    editar restaurado

--- frontend/src/components/utils/Editar.jsx
+++ frontend/src/components/utils/Editar.jsx
@@ -1,18 +1,20 @@
+import { View } from "@react-three/drei";
 import React from "react";
-import axios from "axios";
-
+const images_content = require.context("../../assets/avatar/", true);
 
 //component
 const Editar = ({
     open_,
-    SetEdit,
-    nombre,
-    apellido
+    SetEdit
 }) => {
 
     const [ IsOpen , SetOpen ] = React.useState(true)
-    const [name, setNombre] = React.useState(nombre)
-    const [lastname, seApellido] = React.useState(apellido)
+    const [ IsView , SetView ] = React.useState(false)
+    const [ data_ , setData ] = React.useState({
+        nombre: '',
+        apellido: '',
+        avatar: ''
+    })
     const data_img = [0,1,2,3,4,5,6,7,8,9]
 
     //handles
@@ -21,12 +23,67 @@ const Editar = ({
         alert('hola')
     }
 
-    const onChange = (e) => {
-        console.log(e.target.value)
+    const onChange1 = (e) => {
+        console.log(e.target.value , data_)
+        setData({
+            ...data_,
+            nombre: `${e.target.value}`
+        })
+
+        if (data_.apellido !== '' && data_.avatar !== '' &&
+            (e.target.value).length > 0) {
+            SetOpen(false)
+        }
+        else {
+            SetOpen(true)
+        }
+
+
+    }
+
+    const onChange2 = (e) => {
+        console.log(e.target.value,data_)
+        setData({
+            ...data_,
+            apellido: `${e.target.value}`
+        })
+
+        if (data_.nombre !== '' && data_.avatar !== ''&&
+            (e.target.value).length > 0) {
+            SetOpen(false)
+        }
+        else {
+            SetOpen(true)
+        }
+
     }
 
     const onClosee = () => {
         SetEdit(false)
+    }
+
+    const onViewOption = () => {
+        console.log('a4')
+        SetView(!IsView)
+    }
+
+    const onLeaveMouse = () => {
+        SetView(false)
+    }
+
+    const onClickAvatar = (tap) => {
+        setData({
+            ...data_,
+            avatar: `${tap}`
+        })
+        SetView(false)
+
+        if (data_.nombre !== '' && data_.apellido !== '') {
+            SetOpen(false)
+        }
+        else {
+            SetOpen(true)
+        }
     }
 
     return (
@@ -38,28 +95,32 @@ const Editar = ({
                         <form  onSubmit={onSubmit}  className="email_p">
                         <label>Nombre</label>
                         <input 
-                            defaultValue={name}
+                            placeholder="Nombre"
                             type="text"
-                            onChange={onChange}
+                            onChange={onChange1}
                         />
                         <label>Apellido</label>
                         <input 
-                            value={apellido}
                             placeholder="Apellido"
                             type="text"
-                            onChange={onChange}
+                            onChange={onChange2}
                         />
                         <label>Avatar</label>
-                        <select>
-                            <option defaultChecked disabled> Selecione un Avatar</option>
+                        <div className="container_select">
+                            <div onClick={onViewOption} className="_view_option_more">
+                            <option> { (data_?.avatar !== '')? 'Avatar Selecionado' : 'Selecione un Avatar'}</option>
                             {
-                                data_img?.map( (element,index) => (
-                                    <option value={index} key={index}>
-                                        {element}
-                                    </option>
-                                ))
+                                IsView &&
+                                <div onMouseLeave={onLeaveMouse}  className="container_aux__">
+                                        {data_img?.map( (element,index) => (
+                                            <div onClick={ () => {onClickAvatar(element)}} className="option_select" key={index}>
+                                                <img src={images_content(`./${element}.png`)} />
+                                            </div>
+                                        ))}
+                                </div>
                             }
-                        </select>
+                            </div>
+                        </div>
                         <button className={`${IsOpen? 'disabled_': ''}`} disabled={IsOpen} type="submit"> Editar </button>
                         </form>
                 </section>
